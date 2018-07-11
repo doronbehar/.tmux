a0476d8aea9e6aafb46bc518eca88b11c6be8b00
Doron Behar <doron.behar@gmail.com>
2018-07-11 13:02:10 +0300

    Improve Vim select pane script.
    Make script sh POSIX compatible.

--- vim-select-pane.sh
+++ vim-select-pane.sh
@@ -1,16 +1,32 @@
-#!/bin/bash
+#!/bin/sh
 # Like `tmux select-pane`, but sends a `<C-h/j/k/l>` keystroke if Vim is
 # running in the current pane, or only one pane exists.
-set -e
 
 cmd="$(tmux display -p '#{pane_current_command}')"
 cmd="$(basename "$cmd" | tr A-Z a-z)"
 pane_count="$(printf %d $(tmux list-panes | wc -l))"
 
-if [[ "${cmd%m}" = "vi" || ($pane_count = 1) ]]; then
-  direction="$(echo "${1#-}" | tr 'lLDUR' '\\hjkl')"
+echo "${cmd}" | grep -q 'vi'
+if [ $? == 0 ] || [ $pane_count == 1 ]; then
+  case "$1" in
+    '-L')
+      vim_key='C-h'
+      ;;
+    '-D')
+      vim_key='C-j'
+      ;;
+    '-U')
+      vim_key='C-k'
+      ;;
+    '-R')
+      vim_key='C-l'
+      ;;
+    '-l')
+      vim_key='C-\'
+      ;;
+  esac
   # forward the keystroke to Vim
-  tmux send-keys "C-$direction"
+  tmux send-keys "$vim_key"
 else
   tmux select-pane "$@"
 fi
